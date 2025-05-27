e48b567d3d2734889d517a87e80a45c7eea7ab6f
Niklas Sombert <niklas.sombert@uni-duesseldorf.de>
2025-05-27 18:51:54 +0200

    WIP: virtio-mmio

--- os/kernel/src/device/virtio.rs
+++ os/kernel/src/device/virtio.rs
@@ -5,56 +5,115 @@
 
 use core::ptr::NonNull;
 
-use log::info;
+use log::{error, info};
+use pci_types::{ConfigRegionAccess, PciAddress};
 use virtio_drivers::{
-    transport::{pci::{bus::DeviceFunctionInfo, virtio_device_type, VIRTIO_VENDOR_ID}, DeviceType}, BufferDirection, Hal
+    transport::{
+        pci::{
+            bus::{ConfigurationAccess, DeviceFunction, DeviceFunctionInfo, PciRoot}, virtio_device_type, PciTransport, VIRTIO_VENDOR_ID
+        }, DeviceType, Transport
+    }, BufferDirection, Hal
 };
 
 use crate::pci_bus;
+
+use super::pci::ConfigurationSpace;
 
 /// Search the PCI bus for virtio devices and initialize them.
 // TODO: There are other transports than PCI.
 pub fn init() {
     let devices = pci_bus().search_by_vendor_id(VIRTIO_VENDOR_ID);
-    // we could have used the PCI implementation of virtio_drivers
+    // This is using the PCI implementation of virtio_drivers.
+    // Could this cause conflicts with our one?
+    let mut root = PciRoot::new(PciConfigurationAccess::new(pci_bus().config_space()));
     for device in devices {
-        let lock = device.read();
-        let access = pci_bus().config_space();
-        let (vendor_id, device_id) = lock.header().id(access);
-        let (revision, class, subclass, prog_if) = lock.header().revision_and_class(access);
-        match virtio_device_type(&DeviceFunctionInfo {
-            vendor_id, device_id,
-            class, subclass, prog_if, revision,
-            header_type: type2type(lock.header().header_type(access)),
-        }) {
-            Some(d) => handle_virtio_pci_device(d),
-            None => info!("ignoring virtio device {device_id:x}"),
-        };
+        let bar = device.read().bar(0, pci_bus().config_space());
+        info!("{bar:?}");
+        if let Ok(transport) = PciTransport::new::<VirtioHal, _>(
+            &mut root,
+            address2function(device.read().header().address()),
+        ) {
+            let typ = transport.device_type();
+            info!("found {typ:?} virtio device");
+            match typ {
+                t => info!("ignoring {t:?} virtio device"),
+            }
+        } else {
+            error!(
+                "failed to initialize transport for virtio device at {:?}",
+                device.read().header().address()
+            );
+        }
     }
 }
 
 /// Convert a [`pci_types::HeaderType`] to a [`virtio_drivers::transport::pci::bus::HeaderType`].
 fn type2type(input: pci_types::HeaderType) -> virtio_drivers::transport::pci::bus::HeaderType {
     match input {
-        pci_types::HeaderType::Endpoint => virtio_drivers::transport::pci::bus::HeaderType::Standard,
-        pci_types::HeaderType::PciPciBridge => virtio_drivers::transport::pci::bus::HeaderType::PciPciBridge,
-        pci_types::HeaderType::CardBusBridge => virtio_drivers::transport::pci::bus::HeaderType::PciCardbusBridge,
-        pci_types::HeaderType::Unknown(v) => virtio_drivers::transport::pci::bus::HeaderType::Unrecognised(v),
+        pci_types::HeaderType::Endpoint => {
+            virtio_drivers::transport::pci::bus::HeaderType::Standard
+        }
+        pci_types::HeaderType::PciPciBridge => {
+            virtio_drivers::transport::pci::bus::HeaderType::PciPciBridge
+        }
+        pci_types::HeaderType::CardBusBridge => {
+            virtio_drivers::transport::pci::bus::HeaderType::PciCardbusBridge
+        }
+        pci_types::HeaderType::Unknown(v) => {
+            virtio_drivers::transport::pci::bus::HeaderType::Unrecognised(v)
+        }
         _ => virtio_drivers::transport::pci::bus::HeaderType::Unrecognised(0),
     }
 }
 
-/// Bring up a single virtio-pci device.
-fn handle_virtio_pci_device(typ: DeviceType) {
-    info!("found {typ:?} virtio device");
-    match typ {
-        t => info!("ignoring {t:?} virtio device"),
+/// Convert a [`DeviceFunction`] to a [`PciAddress`].
+fn function2address(function: DeviceFunction) -> PciAddress {
+    PciAddress::new(0x8000, function.bus, function.device, function.function)
+}
+
+/// Convert a [`PciAddress`] to a [`DeviceFunction`].
+fn address2function(address: PciAddress) -> DeviceFunction {
+    assert_eq!(address.segment(), 0x8000);
+    DeviceFunction {
+        bus: address.bus(),
+        device: address.device(),
+        function: address.function(),
     }
 }
 
-struct D3OSHal {}
+struct PciConfigurationAccess<'a> {
+    config_space: &'a ConfigurationSpace,
+}
 
-unsafe impl Hal for D3OSHal {
+impl<'a> PciConfigurationAccess<'a> {
+    fn new(config_space: &'a ConfigurationSpace) -> Self {
+        Self { config_space }
+    }
+}
+
+impl ConfigurationAccess for PciConfigurationAccess<'_> {
+    fn read_word(&self, function: DeviceFunction, offset: u8) -> u32 {
+        unsafe {
+            self.config_space
+                .read(function2address(function), offset.into())
+        }
+    }
+
+    fn write_word(&mut self, function: DeviceFunction, offset: u8, data: u32) {
+        unsafe {
+            self.config_space
+                .write(function2address(function), offset.into(), data)
+        };
+    }
+
+    unsafe fn unsafe_clone(&self) -> Self {
+        todo!()
+    }
+}
+
+struct VirtioHal {}
+
+unsafe impl Hal for VirtioHal {
     fn dma_alloc(
         pages: usize,
         direction: BufferDirection,
@@ -71,7 +130,9 @@ unsafe impl Hal for D3OSHal {
     }
 
     unsafe fn mmio_phys_to_virt(paddr: virtio_drivers::PhysAddr, size: usize) -> NonNull<u8> {
-        todo!()
+        // TODO: check if this is a mmio region
+        // since we have a 1:1 mapping, this is easy
+        NonNull::new(paddr as *mut u8).unwrap()
     }
 
     unsafe fn share(buffer: NonNull<[u8]>, direction: BufferDirection) -> virtio_drivers::PhysAddr {
