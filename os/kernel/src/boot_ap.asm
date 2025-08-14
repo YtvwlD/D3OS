ef901b5e63e14ae08d47a1dd0fd5af17b9a46055
Alex Wantz <exalws@gmail.com>
2025-08-14 21:31:30 +0200

    Completly integrated the boot_bp Code within boot_ap.asm. Did some more refactoring of dead code from the old bootstrap processor Also mixed the descriptions of both files to give a good summary of the assembly Code. Changed the makefile since there is one .asm File less to compile.

--- os/kernel/src/boot_ap.asm
+++ os/kernel/src/boot_ap.asm
@@ -1,30 +1,177 @@
 ;**********************************************************************
 ;*                                                                    *
-;*                  B O O T _ A P                                     *
+;*                  B O O T _ B P                                     *
 ;*                                                                    *
 ;*--------------------------------------------------------------------*
-;* Description:     This is the boot code for all application proc-   *
-;*                  essors. The boostrap processor sends in an IPI    *
-;*                  = Inter-Processor-Interrupt (IPI) in 'startup.rs' * 
-;*                  to all application processors with the addess of  * 
-;*                  the function 'boot_ap'.                           *
-;*                  All application processors start their execution  * 
+;* Description:     ;This is the boot code for all application        *
+;*                  processors. The boostrap processor sends in an    *
+;*                  IPI (= Inter-Processor-Interrupt) in 'boot.rs' to *
+;*                  all application processors with the addess of the *
+;*                  function 'boot_ap'. The bootstrap processor will  *
+;*                  have already done further initialization in Rust  *
+;*                  and relocated the boot code for application       *
+;*                  processors using 'ap_boot_region' and the linker  *
+;*                  labels '___BOOT_AP_START__' & '___BOOT_AP_END__'. *
+;*                                                                    *
+;*                  All application processors start their execution  *
 ;*                  in real mode and we need to switch to protected   *
-;*                  mode first. Afterwards we switch to long mode by  * 
-;*                  calling 'start' in 'boot_bp.asm' which is the     * 
-;*                  entry function for the boot processor. The latter *
-;*                  is switched to protected mode by grub.            *                                      * 
+;*                  mode first. Afterwards we switch to long mode by  +
+;*                  calling the 32 bit protected mode code entry      *
+;*                  function 'start_asm'. It will setup the GDT, page *
+;*                  tables, a stack, switch to long mode and call     *
+;*                  'setup_ap_idt' followed by 'startup_ap',          *
+;*                  the first Rust functions.                         *
+;*                                                                    *
+;*                  We reserve 6400 KB for all stacks. Each processor *
+;*                  gets 400 KB for its stack. This will limit the    *
+;*                  number of supported cores to 16.                  *
 ;*                                                                    *
 ;* Autor:           Michael Schoettner, Univ. Duesseldorf, 25.8.2022  *
+;* Modified by:     Alex Wantz, Univ. Duesseldorf, 14.8.2025          *
 ;**********************************************************************
+
+; Stack
+STACK_MEM_SIZE: equ 6553600	; total size of all stacks (16)
+STACK_SIZE_ONE: equ 409600		; stack size of one processor
+
+; In 'boot.rs' - hier starten die AP's
+[EXTERN startup_ap]
+; In 'interrupt_dispatcher.rs'
+[EXTERN setup_ap_idt]
+; In 'interrupt_dispatcher.rs'
+[EXTERN kernel_cr3]
+
+; In 'linker.ld'
+[EXTERN ___BOOT_AP_START__]     
+[EXTERN ___BOOT_AP_END__]
+
+
+[SECTION .text]
+
+; 32 bit entry function for all application processors
+; Sets up GDT and a stack
+[BITS 32]
+start_asm:
+	cld              ; required by GCC
+	cli              
+	lgdt   [gdt_80]  ; load GDT
+
+	; Set segment registers
+	mov    eax, 3 * 0x8
+	mov    ds, ax
+	mov    es, ax
+	mov    fs, ax
+	mov    gs, ax
+	mov    ss, ax
+
+	; Init stack
+	mov    eax, STACK_SIZE_ONE
+	lock xadd [stack_mem_ptr], eax
+	; Stack grows downwards, thus we need to add STACK_SIZE again
+	add eax, STACK_SIZE_ONE
+	mov    esp, eax
+
+	jmp    init_longmode
+
+
+
 ;
+;  Switch to long mode
+;
+init_longmode:
+	; Activate page address extensions
+	mov    eax, cr4
+	or     eax, 1 << 5
+	mov    cr4, eax
+
+	; Setup paging tables (required for long mode)
+	call set_cr3
+
+	; Switch to long mode (for the time being in compatibility mode)
+	mov    ecx, 0x0C0000080 ; Select Extended Feature Enable Register 
+	rdmsr
+	or     eax, 1 << 8 		; LME (Long Mode Enable)
+	wrmsr
+
+	; Activate paging
+	mov    eax, cr0
+	or     eax, 1 << 31
+	mov    cr0, eax
+
+	; Jump to 64 bit code segment (activates full long mode)
+	jmp    2 * 0x8 : longmode_start
+
+set_cr3:
+    mov eax, [kernel_cr3]
+    mov    cr3, eax
+    ret
+
+
+;
+; Long mode entry function
+;
+; The bootstrap processor will setup the IDT and reprogram PICs
+; The application processors simply only load the same IDT
+longmode_start:
+[BITS 64]
+    ; Sets the idt via setup_ap_idt in interrupt_dispatcher.rs
+	call setup_ap_idt
+
+    ; Call Rust entry function in 'boot.rs' for application proc.
+    call startup_ap
+
+end:
+	; We should never end up here
+    cli
+    mov dword [0xb8000], 0x2f2a2f2a
+	hlt
+
+
+[SECTION .data]
+
+;
+; GDT 
+;
+gdt:
+	dw  0,0,0,0  ; NULL-Deskriptor
+
+	; 32-Bit-Codesegment-Deskriptor
+	dw  0xFFFF   ; 4Gb - (0x100000*0x1000 = 4Gb)
+	dw  0x0000   ; base address=0
+	dw  0x9A00   ; code read/exec
+	dw  0x00CF   ; granularity=4096, 386 (+5th nibble of limit)
+
+	; 64-Bit-Codesegment-Deskriptor
+	dw  0xFFFF   ; 4Gb - (0x100000*0x1000 = 4Gb)
+	dw  0x0000   ; base address=0
+	dw  0x9A00   ; code read/exec
+	dw  0x00AF   ; granularity=4096, 386 (+5th nibble of limit),long mode
+
+	; Datensegment-Deskriptor
+	dw  0xFFFF   ; 4Gb - (0x100000*0x1000 = 4Gb)
+	dw  0x0000   ; base address=0
+	dw  0x9200   ; data read/write
+	dw  0x00CF   ; granularity=4096, 386 (+5th nibble of limit)
+
+gdt_80:
+	dw  4*8 - 1  ; GDT Limit=24, 4 GDT Eintraege - 1
+	dq  gdt      ; Adresse der GDT
+
+;	
+;  Memory for stacks
+;
+stack_mem_ptr:
+	dd  reserve_stack_mem 
+
+[SECTION .bss]
+reserve_stack_mem:
+	resb STACK_MEM_SIZE
+.end:
+
+	
 ; The Segment '.boot_seg_ap' will be relocated by the bootstrap proc.
 ; grub loads the code above 1 MB but real mode code needs to be <1 MB
-;
 [SECTION .boot_seg_ap]
-
-; Entry function of bootstrap processor in 'boot_bp.asm'
-[EXTERN start_asm]
 
 ; 'boot_ap' is the entry function for all application processors
 USE16
@@ -78,9 +225,8 @@ gdtd_ap:
 [SECTION .text]
 
 USE32
-
 ; 32 bit protected mode from here
-; setting up segment registers and call 'start' in 'boot_bp.asm'
+; setting up segment registers and call 'start_asm'
 boot_ap32:
 	mov ax, 0x10
 	mov ds, ax
@@ -90,11 +236,9 @@ boot_ap32:
 	mov ss, ax
 	;hlt
 
-	; In 'boot_bp.asm"
 	jmp start_asm
 
     ; print `**` to screen
     ; should never been reached
     mov dword [0xb8000], 0x2e262e26
 	hlt
-
