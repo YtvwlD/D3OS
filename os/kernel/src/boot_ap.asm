0917379033f254aac48f053cefc3997151f1f2d8
Alex Wantz <exalws@gmail.com>
2025-08-14 21:39:30 +0200

    Rearranged some Code to further increase Readability

--- os/kernel/src/boot_ap.asm
+++ os/kernel/src/boot_ap.asm
@@ -31,8 +31,8 @@
 ;**********************************************************************
 
 ; Stack
-STACK_MEM_SIZE: equ 6553600	; total size of all stacks (16)
-STACK_SIZE_ONE: equ 409600		; stack size of one processor
+STACK_MEM_SIZE: equ 6553600	; totale Groesse für alle AP Stacks (16)
+STACK_SIZE_ONE: equ 409600	; Stack Groesse von einem einzelnem AP
 
 ; In 'boot.rs' - hier starten die AP's
 [EXTERN startup_ap]
@@ -40,13 +40,76 @@ STACK_SIZE_ONE: equ 409600		; stack size of one processor
 [EXTERN setup_ap_idt]
 ; In 'interrupt_dispatcher.rs'
 [EXTERN kernel_cr3]
-
 ; In 'linker.ld'
 [EXTERN ___BOOT_AP_START__]     
 [EXTERN ___BOOT_AP_END__]
 
 
+; The Segment '.boot_seg_ap' will be relocated by the bootstrap proc.
+; grub loads the code above 1 MB but real mode code needs to be <1 MB
+[SECTION .boot_seg_ap]
+
+; 'boot_ap' is the entry function for all application processors
+USE16
+boot_ap:
+	; Initialize segment registers
+	mov ax, cs 		; Same segment for code and data
+	mov ds, ax	 	; For the time being we need no stack
+
+	cli
+	mov al, 0x80
+	out 0x70, al   	; disable NMI
+
+	; Set GDT
+	lgdt [gdtd_ap - boot_ap]
+
+	; Switch to protected mode
+	mov eax, cr0 	; Set PM bit in control register cr0
+	or  eax, 1
+	mov cr0, eax
+
+	; Far jump to load cs
+	jmp dword 0x08:boot_ap32
+
+
+; GDT for application processors (will be replaced later)
+; Attentation: This GDT is in the segment '.boot_seg_ap' because it
+; will be copied below 1 MB during code relocation
+align 4
+
+gdt_ap:
+	dw  0,0,0,0   ; NULL descriptor
+
+	; 32 bit code segment deskriptor
+	dw  0xFFFF    ; 4Gb - (0x100000*0x1000 = 4Gb)
+	dw  0x0000    ; base address=0
+	dw  0x9A00    ; code read/exec
+	dw  0x00CF    ; granularity=4096, 386 (+5th nibble of limit)
+
+	; 32 bit data segment deskriptor
+	dw  0xFFFF    ; 4Gb - (0x100000*0x1000 = 4Gb)
+	dw  0x0000    ; base address=0
+	dw  0x9200    ; data read/write
+	dw  0x00CF    ; granularity=4096, 386 (+5th nibble of limit)
+
+; Limit and address of GDT, needed to load GDT
+gdtd_ap:
+	dw  3*8 - 1   ; GDT Limit=24, 4 GDT entries - 1
+	dd  gdt_ap    ; Adress of GDT
+
+
 [SECTION .text]
+
+USE32
+; 32 bit protected mode from here
+; setting up segment registers and continuing to 'start_asm'
+boot_ap32:
+	mov ax, 0x10
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
 
 ; 32 bit entry function for all application processors
 ; Sets up GDT and a stack
@@ -73,8 +136,6 @@ start_asm:
 
 	jmp    init_longmode
 
-
-
 ;
 ;  Switch to long mode
 ;
@@ -84,8 +145,11 @@ init_longmode:
 	or     eax, 1 << 5
 	mov    cr4, eax
 
+set_cr3:
 	; Setup paging tables (required for long mode)
-	call set_cr3
+    mov eax, [kernel_cr3]
+    mov    cr3, eax
+    ret
 
 	; Switch to long mode (for the time being in compatibility mode)
 	mov    ecx, 0x0C0000080 ; Select Extended Feature Enable Register 
@@ -100,12 +164,6 @@ init_longmode:
 
 	; Jump to 64 bit code segment (activates full long mode)
 	jmp    2 * 0x8 : longmode_start
-
-set_cr3:
-    mov eax, [kernel_cr3]
-    mov    cr3, eax
-    ret
-
 
 ;
 ; Long mode entry function
@@ -167,78 +225,3 @@ stack_mem_ptr:
 reserve_stack_mem:
 	resb STACK_MEM_SIZE
 .end:
-
-	
-; The Segment '.boot_seg_ap' will be relocated by the bootstrap proc.
-; grub loads the code above 1 MB but real mode code needs to be <1 MB
-[SECTION .boot_seg_ap]
-
-; 'boot_ap' is the entry function for all application processors
-USE16
-boot_ap:
-	; Initialize segment registers
-	mov ax, cs 		; Same segment for code and data
-	mov ds, ax	 	; For the time being we need no stack
-
-	cli
-	mov al, 0x80
-	out 0x70, al   	; disable NMI
-
-	; Set GDT
-	lgdt [gdtd_ap - boot_ap]
-
-	; Switch to protected mode
-	mov eax, cr0 	; Set PM bit in control register cr0
-	or  eax, 1
-	mov cr0, eax
-
-	; Far jump to load cs
-	jmp dword 0x08:boot_ap32
-
-
-; GDT for application processors (will be replaced later)
-; Attentation: This GDT is in the segment '.boot_seg_ap' because it
-; will be copied below 1 MB during code relocation
-align 4
-
-gdt_ap:
-	dw  0,0,0,0   ; NULL descriptor
-
-	; 32 bit code segment deskriptor
-	dw  0xFFFF    ; 4Gb - (0x100000*0x1000 = 4Gb)
-	dw  0x0000    ; base address=0
-	dw  0x9A00    ; code read/exec
-	dw  0x00CF    ; granularity=4096, 386 (+5th nibble of limit)
-
-	; 32 bit data segment deskriptor
-	dw  0xFFFF    ; 4Gb - (0x100000*0x1000 = 4Gb)
-	dw  0x0000    ; base address=0
-	dw  0x9200    ; data read/write
-	dw  0x00CF    ; granularity=4096, 386 (+5th nibble of limit)
-
-; Limit and address of GDT, needed to load GDT
-gdtd_ap:
-	dw  3*8 - 1   ; GDT Limit=24, 4 GDT entries - 1
-	dd  gdt_ap    ; Adress of GDT
-
-
-[SECTION .text]
-
-USE32
-; 32 bit protected mode from here
-; setting up segment registers and call 'start_asm'
-boot_ap32:
-	mov ax, 0x10
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov ss, ax
-	;hlt
-
-	jmp start_asm
-
-    ; print `**` to screen
-    ; should never been reached
-    mov dword [0xb8000], 0x2e262e26
-	hlt
