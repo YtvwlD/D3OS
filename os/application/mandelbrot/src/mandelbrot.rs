955323bed6a07a6752ff99d2576e93cdced138b2
Alex Wantz <exalws@gmail.com>
2026-03-01 01:24:27 +0100

    fixed mandelbrot application not working with adjusted parameters - added check for parameters that could crash the os

--- os/application/mandelbrot/src/mandelbrot.rs
+++ os/application/mandelbrot/src/mandelbrot.rs
@@ -23,18 +23,19 @@ const IM_MIN: f64 = -1.2;
 const IM_MAX: f64 = 1.2;
 
 // work-load per job, can adjust overhead
-const ROWS_PER_JOB: u32 = 16;
+const ROWS_PER_JOB_DEFAULT: u32 = 16;
 
 // number of workers (threads)
-const WORKERS: usize = 8;
+const WORKERS_DEFAULT: usize = 8;
 
 // global job-queue
 static NEXT_JOB: AtomicUsize = AtomicUsize::new(0);
 static DONE_WORKERS: AtomicUsize = AtomicUsize::new(0);
 static FB: Once<FramebufferInfo> = Once::new();
 static CID_OUTPUT: AtomicBool = AtomicBool::new(false);
+static ACTIVE_ROWS_PER_JOB: AtomicUsize = AtomicUsize::new(ROWS_PER_JOB_DEFAULT as usize);
 static TOTAL_JOBS: AtomicUsize =
-    AtomicUsize::new(((HEIGHT + ROWS_PER_JOB - 1) / ROWS_PER_JOB) as usize);
+    AtomicUsize::new(((HEIGHT + ROWS_PER_JOB_DEFAULT - 1) / ROWS_PER_JOB_DEFAULT) as usize);
 
 fn mandelbrot(c_re: f64, c_im: f64, max_iter: u32) -> u32 {
     let (mut z_re, mut z_im) = (0.0f64, 0.0f64);
@@ -53,7 +54,7 @@ fn mandelbrot(c_re: f64, c_im: f64, max_iter: u32) -> u32 {
 // returns u32 in the format 0x00RRGGBB
 fn color(it: u32, max_iter: u32) -> u32 {
     if it >= max_iter {
-        return 0x00000000; // schwarz
+        return 0x00000000; // black
     }
     let t = it as f64 / max_iter as f64;
     let r = (9.0  * (1.0 - t) * t * t * t * 255.0) as u32;
@@ -72,7 +73,7 @@ unsafe fn draw_row_block(fb: &FramebufferInfo, y0: u32, y1: u32) {
     for y in y0..y1 {
         let c_im = IM_MAX - (y as f64) * (IM_MAX - IM_MIN) / (HEIGHT as f64);
 
-        // Pointer at start (zentriert)
+        // Pointer at start (centered)
         let row_ptr_u32 = unsafe {
             fb_base.add(((y_off + y) * fb.pitch + x_off * 4) as usize) as *mut u32 };
 
@@ -87,6 +88,7 @@ unsafe fn draw_row_block(fb: &FramebufferInfo, y0: u32, y1: u32) {
 
 fn worker_thread() {
     let fb = FB.get().unwrap();
+    let rows_per_job = ACTIVE_ROWS_PER_JOB.load(Ordering::Acquire) as u32;
 
     if CID_OUTPUT.load(Ordering::Acquire) {
         print!("{} ", current_core_id().unwrap());
@@ -97,8 +99,8 @@ fn worker_thread() {
         if job >= TOTAL_JOBS.load(Ordering::Acquire) {
             break;
         }
-        let y0 = (job as u32) * ROWS_PER_JOB;
-        let y1 = core::cmp::min(y0 + ROWS_PER_JOB, HEIGHT);
+        let y0 = (job as u32) * rows_per_job;
+        let y1 = core::cmp::min(y0 + rows_per_job, HEIGHT);
 
         unsafe { draw_row_block(fb, y0, y1) };
     }
@@ -110,14 +112,13 @@ fn worker_thread() {
 #[unsafe(no_mangle)]
 pub fn main() {
 
-    let mut worker_cnt = WORKERS;
-    let mut rows_per_job = ROWS_PER_JOB;
+    let mut worker_cnt = WORKERS_DEFAULT;
 
     for arg in env::args().skip(1) {
         if let Some(v) = arg.strip_prefix("--workers=") {
-            worker_cnt = v.parse().unwrap_or(worker_cnt);
+            worker_cnt = v.parse().unwrap();
         } else if let Some(v) = arg.strip_prefix("--rows_per_job=") {
-            rows_per_job = v.parse().unwrap_or(rows_per_job);
+            ACTIVE_ROWS_PER_JOB.store(v.parse().unwrap(), Ordering::Release);
         } else if arg.contains("--log-cid") {
             CID_OUTPUT.store(true, Ordering::Release);
         } else {
@@ -125,6 +126,16 @@ pub fn main() {
             println!("Usage: mandelbrot [--workers=<n>] [--rows_per_job=<n>] [--log-cid]");
             return;
         }
+    }
+    let rows_per_job = ACTIVE_ROWS_PER_JOB.load(Ordering::Acquire) as u32;
+
+    if rows_per_job < 1 || rows_per_job > HEIGHT {
+        println!("rows_per_job must be greater than 0 and less than height ({})", HEIGHT);
+        return;
+    }
+    if worker_cnt < 1 {
+        println!("There must be at least one worker thread");
+        return;
     }
 
     let cid_output = CID_OUTPUT.load(Ordering::Acquire);
